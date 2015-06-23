982aeee06022b7103d991e51810116a5cdefde2f
Stephane Bruckert <contact@stephanebruckert.com>
2015-06-24 01:05:30 +0200

    let the user choose his current week plan

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,27 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+	var today = new Date();
+	$scope.dates = [];
+	for (var i = 0; i < 10; i++) {
+		var tomorrow = new Date(today);
+		tomorrow.setDate(today.getDate()+i);
+		$scope.dates.push({date:tomorrow,i:i,icon:"question"});
+	}
 
+	$scope.changeColor = function(day) {
+		for(var i = 0; i < 4; i++) {
+			$scope.dates[(day.i + i) % 10].icon = "home";
+		}
+		for(; i < 6; i++) {
+			$scope.dates[(day.i + i) % 10].icon = "coffee";
+		}
+		for(; i < 8; i++) {
+			$scope.dates[(day.i + i) % 10].icon = "sun-o";
+		}
+		for(; i < 10; i++) {
+			$scope.dates[(day.i + i) % 10].icon = "moon-o";
+		}
+	};
 }]);
