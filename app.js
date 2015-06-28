0963ecc4e18005a694c1b5924ca68f16ec353933
Stephane Bruckert <contact@stephanebruckert.com>
2015-06-29 01:08:08 +0200

    added reference to bower components in app.js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
